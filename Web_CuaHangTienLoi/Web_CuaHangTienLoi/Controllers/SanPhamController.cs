d5166dd684b365e18ff552bce3b7c01b01abccbe
Nguyen Minh Luan <75821049+l2002@users.noreply.github.com>
2023-11-01 16:07:27 +0700

    update Layout, add CSDL ADO
    update Layout, add CSDL ADO

---
+++ Web_CuaHangTienLoi/Web_CuaHangTienLoi/Controllers/SanPhamController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_CuaHangTienLoi.Models;
+
+namespace Web_CuaHangTienLoi.Controllers
+{
+    public class SanphamController : Controller
+    {
+        //
+        // GET: /SanPham/
+
+        QL_CuaHangTienLoi_NewEntities db = new QL_CuaHangTienLoi_NewEntities();
+
+        public ActionResult NuocUong()
+        {
+            var nuoc = db.SANPHAMs.Where(n => n.MALOAI == "LH001").Take(4).ToList();
+            return PartialView(db);
+        }
+
+        public ActionResult BanhKeo()
+        {
+            var nuoc = db.SANPHAMs.Where(n => n.MALOAI == "LH003").Take(4).ToList();
+            return PartialView(db);
+        }
+
+    }
+}
